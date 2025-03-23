04888f5dd1434d8f8afc08b7b70e1b350a8ee873
apun <apun@andrew.cmu.edu>
2025-03-22 20:52:15 -0400

    Add collision check to rejection sampling

--- src/legogpt/data/lego_structure.py
+++ src/legogpt/data/lego_structure.py
@@ -89,7 +89,7 @@ class LegoStructure:
         self.world_dim = world_dim
 
         # Check if structure starts at ground level
-        z0 = min(brick.z for brick in bricks)
+        z0 = min((brick.z for brick in bricks), default=0)
         if z0 != 0:
             warnings.warn('LEGO structure does not start at ground level z=0.')
 
@@ -115,13 +115,21 @@ class LegoStructure:
         self.bricks.append(brick)
         self.voxel_occupancy[brick.slice] += 1
 
+    def undo_add_brick(self) -> None:
+        brick = self.bricks[-1]
+        self.voxel_occupancy[brick.slice] -= 1
+        self.bricks.pop()
+
     def has_collisions(self) -> bool:
         return np.any(self.voxel_occupancy > 1)
 
-    def has_floating_bricks(self) -> bool:
-        return any(self._is_floating(brick) for brick in self.bricks)
+    def brick_collides(self, brick: LegoBrick):
+        return np.any(self.voxel_occupancy[brick.slice])
 
-    def _is_floating(self, brick: LegoBrick) -> bool:
+    def has_floating_bricks(self) -> bool:
+        return any(self.brick_floats(brick) for brick in self.bricks)
+
+    def brick_floats(self, brick: LegoBrick) -> bool:
         if brick.z == 0:
             return False  # Supported by ground
         if np.any(self.voxel_occupancy[*brick.slice_2d, brick.z - 1]):
